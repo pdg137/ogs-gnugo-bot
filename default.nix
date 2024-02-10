cc1638bb2fbd6d7fccdbc4b8441ab52ff143d38d
Paul Grayson <pdg@alum.mit.edu>
2024-02-10 02:00:51 -0500

    updated settings for production

--- default.nix
+++ default.nix
@@ -25,15 +25,22 @@ in
             {
               apikey: "${apikey}",
               engine: "GNU Go 3.8",
+              hidden: true,
               bot: {
                 command: ["${gnugo}/bin/gnugo", "--mode", "gtp"]
+              },
+              greeting: {
+                en: "Hi! I am a bot powered by ${gnugo.name}, ${gtp2ogs.name}, and nixos-${builtins.substring 0 5 pkgs.lib.version}."
+              },
+              farewell: {
+                en: "Thanks for the game!"
               },
             }
           '';
         };
 
     ogs-gnugo-bot = pkgs.writeShellScript "ogs-gnugo-bot" ''
-      ${gtp2ogs}/bin/gtp2ogs -c ${config} --beta
+      ${gtp2ogs}/bin/gtp2ogs -c ${config}
     '';
 
     builder = pkgs.writeShellScript "builder.sh" ''
