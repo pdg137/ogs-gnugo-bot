755d7bc9fdbfd74dfbfab176f887949d4ab3a483
Paul Grayson <pdg@alum.mit.edu>
2024-02-24 13:57:56 -0800

    start resign bot earlier

--- default.nix
+++ default.nix
@@ -26,7 +26,7 @@ with pkgs;
             },
             ending_bot: {
               command: ["${gnugo}/bin/gnugo", "--mode", "gtp"],
-              moves_to_allow_before_checking_ratio: 0.3
+              moves_to_allow_before_checking_ratio: 0.1
             },
             greeting: {
               en: "Hi! I am a bot powered by ${fuego.name}, ${gtp2ogs.name}, and nixos-${builtins.substring 0 5 pkgs.lib.version}."
