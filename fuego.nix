e1ea7ab03ff12edcbba410530cb22ed01f47d243
Paul Grayson <pdg@alum.mit.edu>
2024-02-25 20:47:04 -0800

    fix fuego to build on latest nixpkgs

--- fuego.nix
+++ fuego.nix
@@ -1,21 +1,11 @@
 pkgs:
 let
 
-  # We build with nixpkgs release-15.09, the last one to include boost
-  # 1.57, which is the last verison that seems to work with Fuego.
-  # Also, we could not get it to work with newer C++ compiler versions.
-  old-nixpkgs = import (fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/cc7c26173149348ba43f0799fac3f3823a2d21fc.tar.gz";
-    sha256 = "sha256:1pgsqjw7qfsiivy5hvvslw48mamq5w3zs2jnwaixn657rh509v86";
-  });
-
-  old-pkgs = old-nixpkgs {};
-
-  boost = old-pkgs.boost157;
+  boost = pkgs.boost;
 
 in
 
-  old-pkgs.stdenv.mkDerivation {
+  pkgs.stdenv.mkDerivation {
     name = "fuego-1.1";
 
     buildInputs = [
@@ -39,8 +29,8 @@ in
     # Autoconf does not seem to be able to identify the Boost version
     # without these explicit arguments.
     configureArgs = [
-      "--with-boost=${boost}"
-      "--with-boost-libdir=${boost.lib}/lib"
+      "--with-boost=${boost.dev}"
+      "--with-boost-libdir=${boost}/lib"
     ];
 
     configurePhase = ''
