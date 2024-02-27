427373ba1ae2c95914a3bf38f0d1944b9b694482
Paul Grayson <pdg@alum.mit.edu>
2024-02-27 00:51:09 -0800

    built fuego-r2038

--- fuego.nix
+++ fuego.nix
@@ -6,25 +6,19 @@ let
 in
 
   pkgs.stdenv.mkDerivation {
-    name = "fuego-1.1";
+    name = "fuego-r2038";
 
     buildInputs = [
       boost
+      pkgs.autoconf
+      pkgs.automake
     ];
 
-    src = pkgs.fetchurl {
-      url = "https://master.dl.sourceforge.net/project/fuego/fuego/1.1/fuego-1.1.tar.gz";
-      hash = "sha256-xh0YKTZww83ozIt7KK4DeoWkuAGIJ/Kg+T40sWW8Fs4=";
+    src = pkgs.fetchsvn {
+      url = "https://svn.code.sf.net/p/fuego/code/trunk";
+      rev = "2038";
+      hash = "sha256-ZuPuX+WrW/iRj9hgiOGpY26jFkQY9i7ocDuEi56Y5r8=";
     };
-
-    # Patch to fix a few mistakes that broke compilations. Some
-    # changes might only be necessary for it to work on more recent
-    # nixpkgs versions, which we did not fully get to work.
-    patch = ./fuego-1.1.patch;
-
-    patchPhase = ''
-      patch -p1 < $patch
-      '';
 
     # Autoconf does not seem to be able to identify the Boost version
     # without these explicit arguments.
@@ -34,6 +28,9 @@ in
     ];
 
     configurePhase = ''
+      aclocal
+      autoheader
+      autoreconf -i
       CXXFLAGS=-fpermissive ./configure --prefix=$out $configureArgs
       '';
 
